b7eeb6777ebf5301c161d4ab9e91f4b4e8726dd1
cehv9solutions <cehv9solutions+1@gmail.com>
2024-06-26 16:13:57 +0530

    sast

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-# -*- coding: utf-8 -*-
+# main.py
 
-def rectangle_area(width, height):
-    if width >= 0 and height >= 0:
-        area = width * height
-        status = "Area calculation successful."
-    else:
-        area = None
-        status = "Error: Dimensions must be non-negative."
-
-    return area, status
-
-# Example usage:
-width = 5
-height = 10
-area, status = rectangle_area(width, height)
-if area is not None:
-    print(f"{status} The area of the rectangle with width {width} and height {height} is {area}")
-else:
-    print(status)
+def add(x, y):
+    return x + y
 
 
-# Example usage2:
+def subtract(x, y):
+    return x - y
